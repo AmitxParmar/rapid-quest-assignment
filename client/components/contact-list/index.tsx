20b3c79bee6c47cde03aa88ae93767c9e71eebcb
AmitxParmar <amitparmar901@gmail.com>
2025-08-09 23:07:10 +0530

    complete: searchbar

--- client/components/contact-list/index.tsx
+++ client/components/contact-list/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactHeader from "./contact-header";
+import SearchBar from "./search-bar";
 
 const ContactList = () => {
   return (
     <div className="border-r flex flex-col max-h-screen z-20">
       <ContactHeader />
+      <SearchBar />
       Contact List
     </div>
   );
---
+++ client/components/contact-list/search-bar.tsx
@@ -0,0 +1,24 @@
+import { Search } from "lucide-react";
+import React from "react";
+import { Input } from "../ui/input";
+
+function SearchBar() {
+  return (
+    <div className="flex items-center py-3 px-4 gap-3 h-14">
+      <div className="bg-searchbar flex flex-grow items-center gap-5 px-3 py-1 rounded-full">
+        <div>
+          <Search className="cursor-pointer text-sm" />
+        </div>
+        <div className="w-full">
+          <Input
+            type="text"
+            placeholder="Search or start new chat"
+            className="border-none ring-0 focus:ring-0 focus-visible:ring-0 bg-transparent dark:bg-transparent"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
