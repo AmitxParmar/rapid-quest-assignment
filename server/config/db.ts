c5cbb4ba43408d56e6b41c5118bf3a4c849e9106
AmitxParmar <amitparmar901@gmail.com>
2025-08-11 22:24:51 +0530

    refactor: update server entry point to server.ts, modify build and start scripts, and disable strictQuery in mongoose configuration

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { env } from "./env";
 
 export async function connectDB() {
-  mongoose.set("strictQuery", true);
+  mongoose.set("strictQuery", false);
   mongoose.set("debug", true); // Enable debug mode
 
   try {
