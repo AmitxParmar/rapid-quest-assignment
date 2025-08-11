528d0cf5463b331d98d3975e477bfd0a06aacdbc
AmitxParmar <amitparmar901@gmail.com>
2025-08-12 04:28:23 +0530

    fix: adjust MessageLoader styles for improved appearance and loader size

--- client/components/common/message-loader.tsx
+++ client/components/common/message-loader.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const MessageLoader = () => {
   return (
-    <div className="absolute top-2 left-1/2 -translate-x-1/2 p-0.5 bg-background border rounded-full">
-      <LoaderCircle className="text-label animate-spin p-0.5" />
+    <div className="absolute top-2 left-1/2 -translate-x-1/2 p-1.5 bg-background border rounded-full z-100">
+      <LoaderCircle className="text-label animate-spin p-1" size={30} />
     </div>
   );
 };
